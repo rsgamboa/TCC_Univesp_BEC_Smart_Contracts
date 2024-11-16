41f1b8e800defa847fd30da4f3cd57ea5c22bd48
Rodrigo Sanches Gambôa <rsanchesgamboa@gmail.com>
2024-11-16 16:49:01 +0000

    #8 --update deploy comentarios

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,3 +1,9 @@
+/**
+ * Este script é usado para implantar o contrato inteligente DigitalAttachmentManager.
+ * O contrato DigitalAttachmentManager é responsável por gerenciar anexos digitais
+ * dentro da rede blockchain. Ele lida com a criação, armazenamento e recuperação de
+ * anexos digitais, garantindo sua integridade e acessibilidade.
+ */
 const DigitalAttachmentManager = artifacts.require("DigitalAttachmentManager");
 
 module.exports = function(deployer) {
