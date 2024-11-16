2b20d650ee99c0b73e9f96f14ae49b3c6f5879e2
Rodrigo Sanches Gambôa <rsanchesgamboa@gmail.com>
2024-11-16 16:53:51 +0000

    #10 --update comentarios no truffle

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
+      host: "127.0.0.1", // Endereço IP local onde o nó Ethereum está sendo executado
+      port: 8545, // Porta na qual o nó Ethereum está ouvindo
+      network_id: "*" // Aceita qualquer ID de rede (útil para desenvolvimento)
     }
   },
   compilers: {
     solc: {
-      version: "0.8.0"
+      version: "0.8.0" // Versão do compilador Solidity a ser usada
     }
   }
 };
